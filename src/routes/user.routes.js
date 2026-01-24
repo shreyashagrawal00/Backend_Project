ec79c4985d6293b7fc0f6e0719a67acac7ddbf77
Shreyash Agrawal <shreyash.agrawal3004@gmail.com>
2026-01-24 18:05:45 +0530

    Add RefershToken

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express"; 
-import { loginUser, registerUser } from "../controllers/user.controller.js";
+import { loginUser, registerUser , logoutUser , refreshAccessToken } from "../controllers/user.controller.js";
 import {upload} from '../middlewares/multer.middleware.js';
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -22,6 +22,8 @@ registerUser)
 router.route("/login").post(loginUser);
 
 router.route("/logout").post(verifyJWT,logoutUser);
+
+router.route("/refresh-token").post(refreshAccessToken);
 
 
 
