458d1c32a3e3433959bb7ed528c8d4c730255438
Shreyash Agrawal <shreyash.agrawal3004@gmail.com>
2026-02-02 22:30:48 +0530

    Refactor tweet controller for improved error handling and code clarity

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -1,50 +1,45 @@
-import mongoose, { isValidObjectId } from "mongoose"
-import {Tweet} from "../models/tweet.model.js"
-import {User} from "../models/user.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
-import { error } from "console"
+import mongoose, { isValidObjectId } from "mongoose";
+import { Tweet } from "../models/tweet.models.js";
+import { User } from "../models/user.models.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const createTweet = asyncHandler(async (req, res) => {
-    //TODO: create tweet
-    const {content} = req.body
+  const { content } = req.body;
 
-    if(!content.trim()){
-        throw new ApiError(400,"Please fill the Content||");
-    }
+  if (!content.trim()) {
+    throw new ApiError(400, "Tweet should not be empty.");
+  }
 
-    const user = await User.findById(req.user._id);
-    if (!user) {
+  const user = await User.findById(req.user._id);
+
+  if (!user) {
     throw new ApiError(404, "Author not found");
   }
 
-    const tweet = await Tweet.create({
-        content,
-        owner:user._id,
-    })
+  const tweet = await Tweet.create({
+    content,
+    owner: user._id,
+  });
 
+  const updatedTweet = await tweet.populate("owner","username avatar")
 
-    const updatedTweet = await tweet.populate("owner","username avatar")
-
-    return res
+  return res
     .status(200)
     .json(new ApiResponse(200, updatedTweet, "Tweet created successfully"));
-
-
 });
 
 const getUserTweets = asyncHandler(async (req, res) => {
-    // TODO: get user tweets
 
-    const {userId} = req.params
-    const user = await User.findById(userId);
-    
-    if (!user) {
+  const {userId} = req.params
+  const user = await User.findById(userId);
+
+  if (!user) {
     throw new ApiError(404, "User not found");
-    }
+  }
 
-    const tweets = await Tweet.aggregate([
+  const tweets = await Tweet.aggregate([
     {
       $match: {
         owner: new mongoose.Types.ObjectId(userId),
@@ -83,14 +78,9 @@ const getUserTweets = asyncHandler(async (req, res) => {
   return res
     .status(200)
   .json(new ApiResponse(200,tweets,"All tweets are fetched succefully"))
-
-
-
-})
+});
 
 const updateTweet = asyncHandler(async (req, res) => {
-    //TODO: update tweet
-    const updateTweet = asyncHandler(async (req, res) => {
   const { tweetId } = req.params
   const { content } = req.body
   
@@ -112,11 +102,10 @@ const updateTweet = asyncHandler(async (req, res) => {
     return res.status(200).json(new ApiResponse(200,updatedTweet,"Tweet updated succesfully"))
   }
   throw new ApiError(403,"Permission denied")
-})
+});
 
 const deleteTweet = asyncHandler(async (req, res) => {
-    //TODO: delete tweet
-    const { tweetId } = req.params;
+  const { tweetId } = req.params;
 
   const tweet = await Tweet.findById(tweetId);
   
@@ -129,11 +118,6 @@ const deleteTweet = asyncHandler(async (req, res) => {
      return res.status(200).json(new ApiResponse(200,{},"Tweet deleted successfully"))
    }
    throw new ApiError(403, "Permission denied");
-})
+});
 
-export {
-    createTweet,
-    getUserTweets,
-    updateTweet,
-    deleteTweet
-}
+export { createTweet, getUserTweets, updateTweet, deleteTweet };
